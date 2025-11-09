8927021ea3d0d59fb4d2512b253c4b552d9d91cb
vishnu <edlavishnu@aol.com>
2025-11-09 19:01:47 +0530

    modifed the pattern.py file

--- pythonscripts_for_common_task/pattern.py
+++ pythonscripts_for_common_task/pattern.py
@@ -1,13 +1,13 @@
-def patter_match(pattern):
-    file=open("/home/vishnu/error.log",'r')
+def patter_match(pattern,filename):
+    file=open(filename,'r')
     lines= file.readlines()
     for line in lines:
         if pattern in line:
-            print(f"we found the {pattern}  at line number ")
-            print('Exactly error line in file is',"","", f"{line}")
+            print(f"we found the {pattern}  at {filename}")
+            print('Exactly line in file is' , f"{line}")
             break
         else:
             continue
 
 
-patter_match("error")
+patter_match("error","/home/vishnu/error.log")
